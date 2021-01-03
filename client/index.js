6944cd799f6dbf2f724f79712fd9c59bb3457a0b
Owen Wexler <owenbwexler@gmail.com>
2021-01-02 21:58:54 -0500

    Hook Apollo Client up to front end

--- client/index.js
+++ client/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({});
+
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div><h1>Lyrical</h1></div>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
