3d727c7e9f87eeff1b6ef6587336f647eeaffcda
Owen Wexler <owenbwexler@gmail.com>
2021-01-03 23:19:43 -0500

    Add song title rendering to SongDetail

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -4,13 +4,30 @@ import { graphql } from 'react-apollo';
 
 import getSongByID from '../queries/getSongByID';
 
-const SongDetail = ({ data }) => {
+import Loader from './Loader';
+
+const SongDetail = (props) => {
+
+  console.log('SongDetail props: ', props);
+
+  let pageData;
+
+  if (props.loading || !props.data.song) {
+    pageData = <Loader />
+  } else {
+    const { song } = props.data
+    pageData = <h3 className="center-align"> {song.title} </h3>
+  }
+
   return (
-    <div>
-      <h3>Song Detail</h3>
+    <div className="container">
+      { pageData }
     </div>
+
   )
 }
 
-export default graphql(getSongByID)(SongDetail);
+export default graphql(getSongByID, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
 
